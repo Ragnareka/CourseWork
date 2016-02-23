93635bf5a14f9cd03dfa63045cb91dd657eb716a
Ragnareka <mantikora97@yandex.ru>
2016-02-23 16:42:02 +0300

    подоглала под метабазу выгружаюемую с сайта. надо сделать сортировку по тикеру.
    Signed-off-by: Ragnareka <mantikora97@yandex.ru>

--- structuresbase/structuresbase/structuresbase.cpp
+++ structuresbase/structuresbase/structuresbase.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
+//#include <algorithm>
+
 
 using namespace std;
 
@@ -13,13 +16,13 @@ struct Base // структура данных
 
 {
     char tiker[35];
-	int day;
-	int month;
-	int year;
+	  char per[10];
+	int dateymd;
 	float openPrice;
 	float maxPrice;
 	float minPrice;
 	float closePrice;
+	float volume;
     // остальные элементы структуры  
 	
 };
@@ -27,26 +30,28 @@ struct Base // структура данных
 
 
 int main()
-{  vector <Base> VecBase; //Вектор vecBase содержит структуры типа Base
+{  vector <Base> VecBase; //Вектор vecBase содержит структуры типа Base для работы с данными на данный момент
 ifstream in ("BaseData.txt");
+
 
 Base data; //Чтобы поместить в структуру объект, объект должен существовать
 int n=0;
 while (in.peek()!=EOF)//пока не считан маркер конца файла
 {  VecBase.push_back(data); 
 	in>>VecBase.at(n).tiker;
-	in>>VecBase.at(n).day;
-	in>>VecBase.at(n).month;
-	in>>VecBase.at(n).year;
+	in>>VecBase.at(n).per;
+	in>>VecBase.at(n).dateymd;
 	in>>VecBase.at(n).openPrice;
 	in>>VecBase.at(n).maxPrice;
 	in>>VecBase.at(n).minPrice;
 	in>>VecBase.at(n).closePrice;
+	in>>VecBase.at(n).volume;
 // others
 n++;
 };
 in.close();
 
+ std:: sort(VecBase.begin(), VecBase.end());
 	return 0;
 }
 
